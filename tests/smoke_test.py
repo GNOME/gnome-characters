41f0dc14b240d3e42f000c79c31bc2759f36915c
Daiki Ueno <dueno@src.gnome.org>
2014-09-10 12:14:59 +0900

    tests: Test page switching

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -15,13 +15,52 @@ def active(widget):
 def visible(widget):
     return widget.getState().contains(pyatspi.STATE_VISIBLE)
 
+PAGE_LABELS = [
+    'Punctuations',
+    'Arrows',
+    'Bullets',
+    'Picture',
+    'Currencies',
+    'Math',
+    'Latin',
+    'Emoticons'
+]
+
+class Page(object):
+    def __init__(self, app, label):
+        self.button = app.child(label)
+        self.character_list = app.child('%s Character List' % label)
+
 init()
 try:
     app = start()
     print "app started"
     assert app is not None
 
-    # test something
+    pages = dict()
+    for label in PAGE_LABELS:
+        pages[label] = Page(app, label)
+
+    recently_used_page = Page(app, 'Recently Used')
+    assert recently_used_page.button.showing
+    assert not recently_used_page.character_list.showing
+    pages['Recently Used'] = recently_used_page
+
+    # basic state
+    for label, page in pages.items():
+        assert page.button.showing
+        if label in PAGE_LABELS:
+            assert not page.character_list.showing
+
+    # selection mode
+    for label1 in PAGE_LABELS:
+        page = pages[label1]
+        page.button.click()
+        assert page.character_list.showing
+        for label2 in PAGE_LABELS:
+            if label2 == label1:
+                continue
+            assert not pages[label2].character_list.showing
 finally:
     print "tearing down"
     fini()
