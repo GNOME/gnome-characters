e7a1cedb3081041102b1751290d653de17b4f01a
tommy <tommyshem485@gmail.com>
2018-06-17 15:32:51 +0100

    changed let to const  on none changing refs

--- src/characterList.js
+++ src/characterList.js
@@ -45,7 +45,7 @@ function getCellSize(fontDescription) {
 const CharacterListRow = GObject.registerClass({
 }, class CharacterListRow extends GObject.Object {
     _init(params) {
-        let filtered = Params.filter(params, { characters: null,
+        const filtered = Params.filter(params, { characters: null,
                                                fontDescription: null,
                                                overlayFontDescription: null });
         params = Params.fill(params, {});
@@ -56,7 +56,7 @@ const CharacterListRow = GObject.registerClass({
     }
 
     draw(cr, x, y, width, height) {
-        let layout = PangoCairo.create_layout(cr);
+        const layout = PangoCairo.create_layout(cr);
         layout.set_font_description(this._fontDescription);
 
         // Draw baseline.
@@ -69,8 +69,8 @@ const CharacterListRow = GObject.registerClass({
         cr.setSourceRGBA(0.0, 0.0, 0.0, 1.0);
 
         // Draw characters.  Do centering and attach to the baseline.
-        let cellSize = getCellSize(this._fontDescription);
-        for (let i in this._characters) {
+        const cellSize = getCellSize(this._fontDescription);
+        for (const i in this._characters) {
             var cellRect = new Gdk.Rectangle({ x: x + cellSize * i,
                                                y: y,
                                                width: cellSize,
@@ -81,8 +81,8 @@ const CharacterListRow = GObject.registerClass({
             } else {
                 layout.set_text(this._characters[i], -1);
                 if (layout.get_unknown_glyphs_count () == 0) {
-                    let layoutBaseline = layout.get_baseline();
-                    let [logicalRect, inkRect] = layout.get_extents();
+                    const layoutBaseline = layout.get_baseline();
+                    const [logicalRect, inkRect] = layout.get_extents();
                     cr.moveTo(x + cellSize * i - logicalRect.x / Pango.SCALE +
                               (cellSize - logicalRect.width / Pango.SCALE) / 2,
                               y + BASELINE_OFFSET * height -
@@ -97,14 +97,14 @@ const CharacterListRow = GObject.registerClass({
     }
 
     _computeBoundingBox(cr, cellRect, uc) {
-        let layout = PangoCairo.create_layout(cr);
+        const layout = PangoCairo.create_layout(cr);
         layout.set_font_description(this._fontDescription);
         layout.set_text(uc, -1);
 
         let shapeRect;
         let layoutBaseline;
         if (layout.get_unknown_glyphs_count() == 0) {
-            let [logicalRect, inkRect] = layout.get_extents();
+            const [logicalRect, inkRect] = layout.get_extents();
             layoutBaseline = layout.get_baseline();
             shapeRect = inkRect;
         } else {
@@ -113,7 +113,7 @@ const CharacterListRow = GObject.registerClass({
             // base glyph ('A').
             if (this._baseGlyphRect == null) {
                 layout.set_text('A', -1);
-                let [baseLogicalRect, baseInkRect] = layout.get_extents();
+                const [baseLogicalRect, baseInkRect] = layout.get_extents();
                 this._baseGlyphLayoutBaseline = layout.get_baseline();
                 this._baseGlyphRect = baseInkRect;
             }
@@ -124,7 +124,7 @@ const CharacterListRow = GObject.registerClass({
                 width: this._baseGlyphRect.width,
                 height: this._baseGlyphRect.height,
             });
-            let characterWidth = Gc.character_width (uc);
+            const characterWidth = Gc.character_width (uc);
             if (characterWidth > 1)
                 shapeRect.width *= characterWidth;
         }
@@ -143,12 +143,12 @@ const CharacterListRow = GObject.registerClass({
         cr.rectangle(cellRect.x, cellRect.y, cellRect.width, cellRect.height);
         cr.clip();
 
-        let layout = PangoCairo.create_layout(cr);
+        const layout = PangoCairo.create_layout(cr);
         layout.set_font_description(this._fontDescription);
         layout.set_text(uc, -1);
-        let shapeRect = this._computeBoundingBox(cr, cellRect, uc);
+        const shapeRect = this._computeBoundingBox(cr, cellRect, uc);
 
-        let borderWidth = 1;
+        const borderWidth = 1;
         cr.rectangle(shapeRect.x - borderWidth * 2,
                      shapeRect.y - borderWidth * 2,
                      shapeRect.width + borderWidth * 2,
@@ -164,17 +164,17 @@ const CharacterListRow = GObject.registerClass({
         cr.rectangle(cellRect.x, cellRect.y, cellRect.width, cellRect.height);
         cr.clip();
 
-        let layout = PangoCairo.create_layout(cr);
+        const layout = PangoCairo.create_layout(cr);
         layout.set_width(cellRect.width * Pango.SCALE * 0.8);
         layout.set_height(cellRect.height * Pango.SCALE * 0.8);
         layout.set_wrap(Pango.WrapMode.WORD);
         layout.set_ellipsize(Pango.EllipsizeMode.END);
         layout.set_alignment(Pango.Alignment.CENTER);
         layout.set_font_description(this._overlayFontDescription);
-        let name = Gc.character_name(uc);
-        let text = name == null ? _('Unassigned') : Util.capitalize(name);
+        const name = Gc.character_name(uc);
+        const text = name == null ? _('Unassigned') : Util.capitalize(name);
         layout.set_text(text, -1);
-        let [logicalRect, inkRect] = layout.get_extents();
+        const [logicalRect, inkRect] = layout.get_extents();
         cr.moveTo(cellRect.x - logicalRect.x / Pango.SCALE +
                   (cellRect.width - logicalRect.width / Pango.SCALE) / 2,
                   cellRect.y - logicalRect.y / Pango.SCALE +
@@ -192,13 +192,13 @@ const CharacterListWidget = GObject.registerClass({
     },
 }, class CharacterListWidget extends Gtk.DrawingArea {
     _init(params) {
-        let filtered = Params.filter(params, {
+        const filtered = Params.filter(params, {
             fontDescription: null,
             numRows: NUM_ROWS,
         });
         params = Params.fill(params, {});
         super._init(params);
-        let context = this.get_style_context();
+        const context = this.get_style_context();
         context.add_class('character-list');
         context.save();
         this._cellsPerRow = CELLS_PER_ROW;
@@ -216,15 +216,15 @@ const CharacterListWidget = GObject.registerClass({
     }
 
     vfunc_drag_begin(context) {
-        let cellSize = getCellSize(this._fontDescription);
+        const cellSize = getCellSize(this._fontDescription);
         this._dragSurface = new Cairo.ImageSurface(Cairo.Format.ARGB32,
                                                    cellSize,
                                                    cellSize);
-        let cr = new Cairo.Context(this._dragSurface);
+        const cr = new Cairo.Context(this._dragSurface);
         cr.setSourceRGBA(1.0, 1.0, 1.0, 1.0);
         cr.paint();
         cr.setSourceRGBA(0.0, 0.0, 0.0, 1.0);
-        let row = this._createCharacterListRow([this._character]);
+        const row = this._createCharacterListRow([this._character]);
         row.draw(cr, 0, 0, cellSize, cellSize);
         Gtk.drag_set_icon_surface(context, this._dragSurface, 0, 0);
     }
@@ -235,11 +235,11 @@ const CharacterListWidget = GObject.registerClass({
     }
 
     vfunc_button_press_event(event) {
-        let allocation = this.get_allocation();
-        let cellSize = getCellSize(this._fontDescription);
-        let x = Math.floor(event.x / cellSize);
-        let y = Math.floor(event.y / cellSize);
-        let index = y * this._cellsPerRow + x;
+        const allocation = this.get_allocation();
+        const cellSize = getCellSize(this._fontDescription);
+        const x = Math.floor(event.x / cellSize);
+        const y = Math.floor(event.y / cellSize);
+        const index = y * this._cellsPerRow + x;
         if (index < this._characters.length)
             this._character = this._characters[index];
         else
@@ -258,12 +258,12 @@ const CharacterListWidget = GObject.registerClass({
     }
 
     vfunc_get_preferred_height() {
-        let [minWidth, natWidth] = this.vfunc_get_preferred_width();
+        const [minWidth, natWidth] = this.vfunc_get_preferred_width();
         return this.vfunc_get_preferred_height_for_width(minWidth);
     }
 
     vfunc_get_preferred_height_for_width(width) {
-        let height = Math.max(this._rows.length, this._numRows) *
+        const height = Math.max(this._rows.length, this._numRows) *
             getCellSize(this._fontDescription);
         return [height, height];
     }
@@ -273,17 +273,17 @@ const CharacterListWidget = GObject.registerClass({
     }
 
     vfunc_get_preferred_width_for_height(height) {
-        let cellSize = getCellSize(this._fontDescription);
-        let minWidth = NUM_COLUMNS * cellSize;
-        let natWidth = Math.max(this._cellsPerRow, NUM_COLUMNS) * cellSize;
+        const cellSize = getCellSize(this._fontDescription);
+        const minWidth = NUM_COLUMNS * cellSize;
+        const natWidth = Math.max(this._cellsPerRow, NUM_COLUMNS) * cellSize;
         return [minWidth, natWidth];
     }
 
     vfunc_size_allocate(allocation) {
         super.vfunc_size_allocate(allocation);
 
-        let cellSize = getCellSize(this._fontDescription);
-        let cellsPerRow = Math.floor(allocation.width / cellSize);
+        const cellSize = getCellSize(this._fontDescription);
+        const cellsPerRow = Math.floor(allocation.width / cellSize);
         if (cellsPerRow != this._cellsPerRow) {
             // Reflow if the number of cells per row has changed.
             this._cellsPerRow = cellsPerRow;
@@ -295,7 +295,7 @@ const CharacterListWidget = GObject.registerClass({
         var context = this.get_pango_context();
         var fontDescription = context.get_font_description();
         fontDescription.set_size(fontDescription.get_size() * 0.8);
-        let row = new CharacterListRow({
+        const row = new CharacterListRow({
             characters: characters,
             fontDescription: this._fontDescription,
             overlayFontDescription: fontDescription,
@@ -314,15 +314,15 @@ const CharacterListWidget = GObject.registerClass({
         let start = 0, stop = 1;
         for (; stop <= characters.length; stop++) {
             if (stop % this._cellsPerRow == 0) {
-                let rowCharacters = characters.slice(start, stop);
-                let row = this._createCharacterListRow(rowCharacters);
+                const rowCharacters = characters.slice(start, stop);
+                const row = this._createCharacterListRow(rowCharacters);
                 this._rows.push(row);
                 start = stop;
             }
         }
         if (start != stop - 1) {
-            let rowCharacters = characters.slice(start, stop);
-            let row = this._createCharacterListRow(rowCharacters);
+            const rowCharacters = characters.slice(start, stop);
+            const row = this._createCharacterListRow(rowCharacters);
             this._rows.push(row);
         }
 
@@ -332,9 +332,9 @@ const CharacterListWidget = GObject.registerClass({
 
     vfunc_draw(cr) {
         // Clear the canvas.
-        let context = this.get_style_context();
-        let fg = context.get_color(Gtk.StateFlags.NORMAL);
-        let bg = context.get_background_color(Gtk.StateFlags.NORMAL);
+        const context = this.get_style_context();
+        const fg = context.get_color(Gtk.StateFlags.NORMAL);
+        const bg = context.get_background_color(Gtk.StateFlags.NORMAL);
 
         cr.setSourceRGBA(bg.red, bg.green, bg.blue, bg.alpha);
         cr.paint();
@@ -343,13 +343,13 @@ const CharacterListWidget = GObject.registerClass({
         // Use device coordinates directly, since PangoCairo doesn't
         // work well with scaled matrix:
         // https://bugzilla.gnome.org/show_bug.cgi?id=700592
-        let allocation = this.get_allocation();
+        const allocation = this.get_allocation();
 
         // Redraw rows within the clipped region.
-        let [x1, y1, x2, y2] = cr.clipExtents();
-        let cellSize = getCellSize(this._fontDescription);
-        let start = Math.max(0, Math.floor(y1 / cellSize));
-        let end = Math.min(this._rows.length, Math.ceil(y2 / cellSize));
+        const [x1, y1, x2, y2] = cr.clipExtents();
+        const cellSize = getCellSize(this._fontDescription);
+        const start = Math.max(0, Math.floor(y1 / cellSize));
+        const end = Math.min(this._rows.length, Math.ceil(y2 / cellSize));
         for (let index = start; index < end; index++) {
             this._rows[index].draw(cr, 0, index * cellSize,
                                    allocation.width, cellSize);
@@ -375,7 +375,7 @@ var FontFilter = GObject.registerClass({
     }
 
     set font(v) {
-        let fontDescription = Pango.FontDescription.from_string(v);
+        const fontDescription = Pango.FontDescription.from_string(v);
         if (fontDescription.get_size() == 0)
             fontDescription.set_size(CELL_SIZE * Pango.SCALE);
 
@@ -424,11 +424,11 @@ var FontFilter = GObject.registerClass({
     apply(widget, characters) {
         let fontDescription = this._fontDescription;
         if (this._filterFontDescription) {
-            let context = widget.get_pango_context();
-            let filterFont = context.load_font(this._filterFontDescription);
-            let filteredCharacters = [];
+            const context = widget.get_pango_context();
+            const filterFont = context.load_font(this._filterFontDescription);
+            const filteredCharacters = [];
             for (let index = 0; index < characters.length; index++) {
-                let uc = characters[index];
+                const uc = characters[index];
                 if (Gc.pango_context_font_has_glyph(context, filterFont, uc))
                     filteredCharacters.push(uc);
             }
@@ -448,7 +448,7 @@ var CharacterListView = GObject.registerClass({
     },
 }, class CharacterListView extends Gtk.Stack {
     _init(params) {
-        let filtered = Params.filter(params, {
+        const filtered = Params.filter(params, {
             fontFilter: null,
         });
         params = Params.fill(params, {
@@ -467,12 +467,12 @@ var CharacterListView = GObject.registerClass({
                                     Lang.bind(this, function(w, c) {
                                         this.emit('character-selected', c);
                                     }));
-        let scroll = new Gtk.ScrolledWindow({
+        const scroll = new Gtk.ScrolledWindow({
             hscrollbar_policy: Gtk.PolicyType.NEVER,
             visible: true,
         });
         scroll.add(this._characterList);
-        let context = scroll.get_style_context();
+        const context = scroll.get_style_context();
         context.add_class('character-list-scroll');
         context.save();
         this.add_named(scroll, 'character-list');
@@ -517,7 +517,7 @@ var CharacterListView = GObject.registerClass({
     _finishSearch(result) {
         this._stopSpinner();
 
-        let characters = Util.searchResultToArray(result);
+        const characters = Util.searchResultToArray(result);
 
         this.setCharacters(characters);
     }
@@ -528,7 +528,7 @@ var CharacterListView = GObject.registerClass({
     }
 
     _updateCharacterList() {
-        let [fontDescription, characters] = this._fontFilter.apply(this, this._characters);
+        const [fontDescription, characters] = this._fontFilter.apply(this, this._characters);
         this._characterList.setFontDescription(fontDescription);
         this._characterList.setCharacters(characters);
         if (characters.length == 0) {
@@ -554,15 +554,15 @@ var CharacterListView = GObject.registerClass({
     get initialSearchCount() {
         // Use our parents allocation; we aren't visible before we do the
         // initial search, so our allocation is 1x1
-        let allocation = this.get_parent().get_allocation();
+        const allocation = this.get_parent().get_allocation();
 
         // Sometimes more MAX_SEARCH_RESULTS are visible on screen
         // (eg. fullscreen at 1080p).  We always present a over-full screen,
         // otherwise the lazy loading gets broken
-        let cellSize = getCellSize(this._fontFilter.fontDescription);
-        let cellsPerRow = Math.floor(allocation.width / cellSize);
+        const cellSize = getCellSize(this._fontFilter.fontDescription);
+        const cellsPerRow = Math.floor(allocation.width / cellSize);
         // Ensure the rows cause a scroll
-        let heightInRows = Math.ceil((allocation.height + 1) / cellSize);
+        const heightInRows = Math.ceil((allocation.height + 1) / cellSize);
 
         return Math.max(MAX_SEARCH_RESULTS, heightInRows * cellsPerRow);
     }
@@ -574,7 +574,7 @@ var CharacterListView = GObject.registerClass({
     }
 
     _addSearchResult(result) {
-        let characters = Util.searchResultToArray(result);
+        const characters = Util.searchResultToArray(result);
         this.setCharacters(this._characters.concat(characters));
     }
 
@@ -586,7 +586,7 @@ var CharacterListView = GObject.registerClass({
             Lang.bind(this, function(context, res, user_data) {
                 this._stopSpinner();
                 try {
-                    let result = context.search_finish(res);
+                    const result = context.search_finish(res);
                     this._addSearchResult(result);
                 } catch (e) {
                     log(`Failed to search: ${e.message}`);
@@ -600,13 +600,13 @@ var CharacterListView = GObject.registerClass({
             return;
         }
 
-        let criteria = Gc.SearchCriteria.new_category(category.category);
+        const criteria = Gc.SearchCriteria.new_category(category.category);
         this._searchContext = new Gc.SearchContext({ criteria: criteria });
         this._searchWithContext(this._searchContext, this.initialSearchCount);
     }
 
     searchByKeywords(keywords) {
-        let criteria = Gc.SearchCriteria.new_keywords(keywords);
+        const criteria = Gc.SearchCriteria.new_keywords(keywords);
         this._searchContext = new Gc.SearchContext({
             criteria: criteria,
             flags: Gc.SearchFlag.WORD,
@@ -632,7 +632,7 @@ var RecentCharacterListView = GObject.registerClass({
     },
 }, class RecentCharacterListView extends Gtk.Bin {
     _init(params) {
-        let filtered = Params.filter(params, {
+        const filtered = Params.filter(params, {
             category: null,
             fontFilter: null,
         });
@@ -662,13 +662,13 @@ var RecentCharacterListView = GObject.registerClass({
     }
 
     setCharacters(characters) {
-        let result = Gc.filter_characters(this._category, characters);
+        const result = Gc.filter_characters(this._category, characters);
         this._characters = Util.searchResultToArray(result);
         this._updateCharacterList();
     }
 
     _updateCharacterList() {
-        let [fontDescription, characters] = this._fontFilter.apply(this, this._characters);
+        const [fontDescription, characters] = this._fontFilter.apply(this, this._characters);
         this._characterList.setFontDescription(fontDescription);
         this._characterList.setCharacters(characters);
         this.show_all();
