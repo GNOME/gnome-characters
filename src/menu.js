b44a3182bf16503c566d46a04cef49325ed04955
tommy <tommyshem485@gmail.com>
2018-06-17 16:56:45 +0100

    gi imports to one line

--- src/menu.js
+++ src/menu.js
@@ -16,9 +16,8 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-const GLib = imports.gi.GLib;
-const GObject = imports.gi.GObject;
-const Gtk = imports.gi.Gtk;
+const {GLib, GObject, Gtk} = imports.gi;
+
 const Lang = imports.lang;
 const Params = imports.params;
 
