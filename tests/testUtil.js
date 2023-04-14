0c3c3076e9b96e11e273b795f6d5c99dd2635a52
Marco Trevisan (Treviño) <mail@3v1n0.net>
2023-04-14 03:52:00 +0200

    tests: Add basic test for Gc.Search and Util.searchResultToArray

--- tests/testUtil.js
+++ tests/testUtil.js
@@ -3,7 +3,7 @@ imports.gi.versions.Gtk = '4.0';
 
 const { jsUnit: JsUnit } = imports;
 const { util: Util } = imports;
-const { GLib, Gsk, Gtk } = imports.gi;
+const { Gc, GLib, Gsk, Gtk } = imports.gi;
 
 Gtk.init();
 
@@ -13,3 +13,28 @@ emojis.forEach(e => JsUnit.assertTrue(
 
 const whiteSpaces = ['', ' ', '\n', '\r', '\n', '\t', '\t   \n    \n \r      '];
 whiteSpaces.forEach(e => JsUnit.assertNull(Util.characterToIconData(e)));
+
+function testSearch(keywords, maxResults = 5) {
+    const upper = keywords.split(' ').map(x => x.toUpperCase());
+    const criteria = Gc.SearchCriteria.new_keywords(upper);
+    const context = new Gc.SearchContext({ criteria, flags: Gc.SearchFlag.WORD });
+    const loop = new GLib.MainLoop(null, false);
+
+    const characters = [];
+    context.search(maxResults, null, (_source, res) => {
+        try {
+            const result = context.search_finish(res);
+            characters.push(...Util.searchResultToArray(result));
+        } catch (e) {
+            throw e;
+        } finally {
+            loop.quit();
+        }
+    });
+
+    loop.run();
+    return characters;
+}
+
+JsUnit.assertTrue(testSearch('pizza').includes('🍕'));
+JsUnit.assertTrue(testSearch('joy').includes('😂'));
