100524cf888fff3c78e806ff21beec38b49bc8d2
Alice Mikhaylenko <alicem@gnome.org>
2024-09-12 03:11:15 +0400

    lib: Update to Emoji 16.0

--- lib/regenerate.sh
+++ lib/regenerate.sh
@@ -2,7 +2,7 @@
 
 CLDR_VERSION=45
 UCD_VERSION=16.0.0
-EMOJI_VERSION=15.1
+EMOJI_VERSION=16.0
 
 wget -c "https://www.unicode.org/Public/$UCD_VERSION/ucd/Blocks.txt"
 wget -c "https://www.unicode.org/Public/$UCD_VERSION/ucd/Jamo.txt"
